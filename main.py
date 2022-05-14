90f9feb03cc8814f2d4374b61e4fdbf9f4d327d3
Sergey9089 <103736024+Sergey9089@users.noreply.github.com>
2022-05-14 07:02:34 +0500

    my first non-bug program
    my first non-bug program for solving a logic problem

---
+++ main.py
@@ -0,0 +1,54 @@
+def ex():
+    money = None
+    while type(money) != int:
+        try:
+            money = int(input('Некорректный тип. Введите целое число: '))
+        except ValueError:
+            continue
+    return money
+ans = input('Вычислить год/деньги ')
+ans = ans.lower()
+if ans != 'год' and ans != 'деньги':
+    while ans != 'год' or ans != 'деньги':
+        ans = input('Неверное значение. Введите заново: ').lower()
+        if ans == 'год' or ans == 'деньги':
+            break
+if ans == 'год':
+    try:
+        money = int(input('Деньги: '))
+    except ValueError:
+        money = ex()
+    years = 1
+    plus = 1
+    summ = 1
+    for i in range(money):
+        years += 1
+        plus += 1
+        summ += plus
+        if summ == money:
+            break
+        if summ > money:
+            try:
+                money = int(input('Неверное число. Введите другое: '))
+            except ValueError:
+                money = ex()
+            years = 1
+            plus = 1
+            summ = 1
+    print(years, 'лет')
+if ans == 'деньги':
+    try:
+        years = int(input('Года: '))
+    except ValueError:
+        years = None
+        while type(years) != int:
+            try:
+                years = int(input('Некорректный тип. Введите целое число: '))
+            except ValueError:
+                continue
+    plus = 1
+    money = 0
+    for i in range(years):
+        money += plus
+        plus += 1
+    print(money, 'денег')
