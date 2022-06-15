a79bfc9120a72ec8253ab92cdd5f8092169a8a9b
Sergey9089 <103736024+Sergey9089@users.noreply.github.com>
2022-06-15 20:32:38 +0500

    little update
    now the numbers in the program adapt to any number of files in the folder

--- Japanese_opener.py
+++ Japanese_opener.py
@@ -1,9 +1,9 @@
 from os import startfile, path, walk
 from random import shuffle
-def exec(number):
-    while number > 14:
+def exec(number, len_files):
+    while number > len_files - 1:
         try:
-            number = int(input('Число слишком большое. Максимум 14 '))
+            number = int(input(f'Число слишком большое. Максимум {len_files - 1} '))
         except ValueError:
             number = None
             while type(number) != int:
@@ -23,7 +23,12 @@ if ans == 'выбор' or ans == '1':
     print('Темы:')
     for adress, dirs, files in walk("C://Users/home/Desktop/Японский"):
         for file in files:
-            print(file[:-5])
+            count = 0
+            for symbols in file:
+                count += 1
+                if '.' in symbols:
+                    break
+            print(file[:count - 1])
     files_sp = input('Какие файлы хотите открыть? ').lower().split()
     if 'os.system' in files_sp or 'os.popen' in files_sp or 'os.exec' in files_sp:
         quit('Доступ заблокирован')
@@ -37,26 +42,31 @@ if ans == 'выбор' or ans == '1':
         while test == False:
             files_sp.clear()
             files_sp = input('Не было найдено файлов с таким названием. Введите заново: ').lower().split()
+            if 'os.system' in files_sp or 'os.popen' in files_sp or 'os.exec' in files_sp:
+                quit('Доступ заблокирован')
             for file in files:
                 for i in range(len(files_sp)):
                     if files_sp[i] in file.lower():
                         startfile(path.join(adress, file))
                         test = True
 if ans == 'рандом' or ans == '2':
+    len_files = 0
     try:
-        number = int(input('Сколько файлов открыть?(макс 14) '))
-        number = exec(number)
+        for adress, dirs, files in walk("C://Users/home/Desktop/Японский"):
+            len_files = len(files)
+        number = int(input(f'Сколько файлов открыть?(макс {len_files - 1}) '))
+        number = exec(number, len_files)
     except ValueError:
         number = None
         while type(number) != int:
             try:
                 number = int(input('Неверное значение. Введите заново: '))
-                number = exec(number)
+                number = exec(number, len_files)
             except ValueError:
                 continue
-    id_1 = list(range(0, 15))
+    id_1 = list(range(0, len_files))
     shuffle(id_1)
-    num_files = id_1[:(number - 15)]
+    num_files = id_1[:(number - len_files)]
     for adress, dirs, files in walk("C://Users/home/Desktop/Японский"):
         for i in range(len(files)):
             if files.index(files[i]) in num_files:
