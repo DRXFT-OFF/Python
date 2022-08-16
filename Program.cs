7b67a79ea073c25c9a963b355b34c7703cca6948
Sergey9089 <103736024+Sergey9089@users.noreply.github.com>
2022-08-16 14:46:58 +0500

    program for counting the number of characters

--- Program.cs
+++ Program.cs
@@ -10,104 +10,22 @@ namespace main
     {
         static void Main(string[] args)
         {
-            string morze = Console.ReadLine();
-            morze += " ";
-            string now = "";
+            Console.WriteLine("Введите строку для подсчёта символов");
 
-            for (int i = 0; i < morze.Length; i++)
+            string str = Console.ReadLine();
+            uint words = 0;
+            uint wosymb = Convert.ToUInt32(str.Length);
+
+            for (int i = 0; i < str.Length; i++)
             {
-                now += morze[i];
-
-                if (morze[i] == ' ')
+                if (str[i] == ' ')
                 {
-                    switch (now)
-                    {
-                        case ".- ":
-                            Console.WriteLine("A");
-                            break;
-                        case "-... ":
-                            Console.WriteLine("B");
-                            break;
-                        case "-.-. ":
-                            Console.WriteLine("C");
-                            break;
-                        case "-.. ":
-                            Console.WriteLine("D");
-                            break;
-                        case ". ":
-                            Console.WriteLine("E");
-                            break;
-                        case "..-. ":
-                            Console.WriteLine("F");
-                            break;
-                        case "--. ":
-                            Console.WriteLine("G");
-                            break;
-                        case ".... ":
-                            Console.WriteLine("H");
-                            break;
-                        case ".. ":
-                            Console.WriteLine("I");
-                            break;
-                        case ".--- ":
-                            Console.WriteLine("J");
-                            break;
-                        case "-.- ":
-                            Console.WriteLine("K");
-                            break;
-                        case ".-.. ":
-                            Console.WriteLine("L");
-                            break;
-                        case "-- ":
-                            Console.WriteLine("M");
-                            break;
-                        case "-. ":
-                            Console.WriteLine("N");
-                            break;
-                        case "--- ":
-                            Console.WriteLine("O");
-                            break;
-                        case ".--. ":
-                            Console.WriteLine("P");
-                            break;
-                        case "--.- ":
-                            Console.WriteLine("Q");
-                            break;
-                        case ".-. ":
-                            Console.WriteLine("R");
-                            break;
-                        case "... ":
-                            Console.WriteLine("S");
-                            break;
-                        case "- ":
-                            Console.WriteLine("T");
-                            break;
-                        case "..- ":
-                            Console.WriteLine("U");
-                            break;
-                        case "...- ":
-                            Console.WriteLine("V");
-                            break;
-                        case ".-- ":
-                            Console.WriteLine("W");
-                            break;
-                        case "-..- ":
-                            Console.WriteLine("X");
-                            break;
-                        case "-.-- ":
-                            Console.WriteLine("Y");
-                            break;
-                        case "--.. ":
-                            Console.WriteLine("Z");
-                            break;
-
-                        default:
-                            break;
-                    }
-                    now = "";
-                    continue;
+                    wosymb -= 1;
+                    words += 1;
                 }
             }
+            Console.WriteLine($"Слова: {words+1} \nСимволы: {str.Length} \nСимволы без пробелов: {wosymb}");
+
             Console.ReadLine();
         }
     }
